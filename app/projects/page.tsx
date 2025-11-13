fe63bb6b23c2ad57437f9eb11fb01ad48b447795
dipanshuhappy <dipanshuhappy@gmail.com>
2025-11-13 17:22:29 +0530

    Comment out Projects component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Projects</h1>
-      <Projects />
+      {/*<Projects />*/}
     </section>
   )
 }
